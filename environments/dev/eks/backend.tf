fa85a561df5304cfc665acb2c5865d9a45c47762
champ886 <champion_nweke@yahoo.com>
2026-05-07 23:06:54 +1000

    added assume role to eks backend

--- environments/dev/eks/backend.tf
+++ environments/dev/eks/backend.tf
@@ -3,8 +3,11 @@ terraform {
     bucket         = "tf-state-landing-zone-champ-001"
     key            = "aws-lza/dev/eks/terraform.tfstate"
     region         = "ap-southeast-2"
-    dynamodb_table = "tf-locks"
     encrypt        = true
-    role_arn       = "arn:aws:iam::501562869247:role/TerraformStateRole"
+    use_lockfile   = true
+
+    assume_role = {
+      role_arn = "arn:aws:iam::501562869247:role/TerraformStateRole"
+    }
   }
 }
