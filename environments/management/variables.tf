fb44c9786694d0f50693e6b87a88470008b1b3de
champ886 <champion_nweke@yahoo.com>
2026-05-07 22:42:22 +1000

    added org id as default to mgmt variables

--- environments/management/variables.tf
+++ environments/management/variables.tf
@@ -26,6 +26,7 @@ variable "environment" {
 variable "org_id" {
   description = "AWS Organization ID"
   type        = string
+  default     = "o-mrik3s6w85"
 }
 
 # -----------------------------------------------
